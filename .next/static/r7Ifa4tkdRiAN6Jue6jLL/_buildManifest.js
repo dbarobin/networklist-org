745f48d2fdb573c2681a673d20bb499071e29b03
Robin Wen <blockxyz@gmail.com>
2022-03-06 01:30:14 +0000

    Fix

--- .next/static/r7Ifa4tkdRiAN6Jue6jLL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F080c452e6db8417972a9.css","static\u002Fchunks\u002Fpages\u002Findex-d5efe4f5449421ee6798.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f174a6d35af3affe4692.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
